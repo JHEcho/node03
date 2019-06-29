558e15ed270a17516ad31ffaa29b1a1526b666e6
ShimJaewook <ts4837829@naver.com>
2019-06-29 11:34:52 +0900

    1

--- src/components/InfoBanner/InfoBanner.js
+++ src/components/InfoBanner/InfoBanner.js
@@ -5,7 +5,7 @@ export default function InfoBannerComponent() {
   return (
     <div className="info-banner">
       <div className="row grid-default-style">
-      <div className="col-6">column 01</div>
+      <div className="col-3">column 01</div>
       <div className="col-6">column 02</div>
       </div>
     </div>
