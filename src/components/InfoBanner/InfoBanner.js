b54906fd2d52ad1a8fb6620aaf9f9fd51d6edb34
ShimJaewook <ts4837829@naver.com>
2019-06-29 11:27:43 +0900

    1--1

--- src/components/InfoBanner/InfoBanner.js
+++ src/components/InfoBanner/InfoBanner.js
@@ -4,7 +4,10 @@ import './InfoBanner.scss';
 export default function InfoBannerComponent() {
   return (
     <div className="info-banner">
-      (Info Banner)
+      <div className="row grid-default-style">
+      <div className="col-6">column 01</div>
+      <div className="col-6">column 02</div>
+      </div>
     </div>
   );
 }
